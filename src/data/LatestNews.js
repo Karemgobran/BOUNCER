10fe526c7bc6bb2d6d22492d5d5eaf6c0d57be74
Karem Gobran <112480829+Karemgobran@users.noreply.github.com>
2025-02-13 16:45:17 +0200

    add news data

---
+++ src/data/LatestNews.js
@@ -0,0 +1,32 @@
+import img_1 from "../assets/images/Rectangle1.png";
+import img_2 from "../assets/images/Rectangle2.png";
+import img_3 from "../assets/images/Rectangle3.png";
+
+const LatestNewsData = [
+  {
+    id: 3,
+    name: "Google Pixel 8 Pro",
+    releaseDate: "2023-10-04",
+    details:
+      "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus temporibus impedit  ",
+    img: img_1,
+  },
+  {
+    id: 4,
+    name: "OnePlus 12",
+    releaseDate: "2024-01-23",
+    details:
+      "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus temporibus impedit  ",
+    img: img_2,
+  },
+  {
+    id: 5,
+    name: "Xiaomi 14 Pro",
+    releaseDate: "2023-12-10",
+    details:
+      "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus temporibus impedit  ",
+    img: img_3,
+  },
+];
+
+export default LatestNewsData;
