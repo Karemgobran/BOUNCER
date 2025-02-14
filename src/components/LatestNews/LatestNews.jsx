94926e545c47ef177109fee456086a99d8ceaf00
Karem Gobran <112480829+Karemgobran@users.noreply.github.com>
2025-02-15 01:39:10 +0200

    Converted SCSS styles to Bootstrap classes in latest news

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -5,15 +5,18 @@ function LatestNews() {
   return (
     <>
       <div className="title container">
-        <h2>Latest News</h2>
+        <h2 className="text-center">Latest News</h2>
       </div>
       <div className="cards container">
         {LatestNewsData.map((product) => (
-          <div className="card" key={product.id}>
+          <div className="card d-flex flex-row" key={product.id}>
             <div className="image ">
               <img src={product.img} alt="news" />
             </div>
-            <div className="news" key={product.id}>
+            <div
+              className="news d-flex flex-column justify-content-between"
+              key={product.id}
+            >
               <span>{product.releaseDate}</span>
               <p className="name">{product.name}</p>
               <p className="details">{product.details}</p>
