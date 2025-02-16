195a50f8dd2c28e13fb8cbe122430b40853a1290
Karem Gobran <112480829+Karemgobran@users.noreply.github.com>
2025-02-16 21:50:37 +0200

    Made footer persistent across all pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import ACCESORIES from "./pages/ACCESORIES/ACCESORIES";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/ACCESORIES" element={<ACCESORIES />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
