261456006f31da5009fbefb994af239cccd551e5
Karem Gobran <112480829+Karemgobran@users.noreply.github.com>
2025-02-16 18:53:31 +0200

    add accesorices page

--- src/pages/ACCESORIES/ACCESORIES.jsx
+++ src/pages/ACCESORIES/ACCESORIES.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import FilterSidebar from "../../components/FilterSidebar/FilterSidebar";
-
+import Broducts from "../../components/Broducts/Broducts";
+import "./ACCESORIES.scss";
 function ACCESORIES() {
   return (
     <>
-      <div className="row container">
-        <div className="col-3">
-          <FilterSidebar />
+      <div className="container mt-5">
+        <div className="row ">
+          <div className="col-lg-3 col-md-4 col-12">
+            <FilterSidebar />
+          </div>
+          <div className="col-lg-9 col-md-8 col-12">
+            <Broducts />
+          </div>
         </div>
-        <div className="col-9">Gobran</div>
       </div>
     </>
   );
